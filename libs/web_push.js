163365cbf05172cbe63407090598b21430375c2f
Globik <globalikslivov@gmail.com>
2022-02-04 12:18:53 +0500

    fuck

--- libs/web_push.js
+++ libs/web_push.js
@@ -12,16 +12,13 @@ let data = {
 		contents: {en: us + " " + txt},
 	included_segments: ["Subscribed Users"],
 		//include_player_ids: ["9a9c34d6-6c6e-4dfe-b510-20953def482f"],
-		data:{"hallo": "world!"},
-		web_buttons: [{"id": "like-button", "text": "Like", "icon": "https://chelikon.space/images/ich.jpg", "url": "https://chelikon.space"}, 
-		{"id": "read-more-button", "text": "Read more", "icon": "https://chelikon.space/images/eye2.svg", "url": "https://chelikon.space"}]
 		};
 let headers = { "Authorization": "Basic " + onesignal_app_key };
 try{
 let r = await axios.post(onesignal_notification_url, data, { headers: headers });
 console.log("r: ", r.data);
 }catch(e){
-console.log("err: ", e);
+console.log("err: ", e.toString());
 }	
 }
 }
